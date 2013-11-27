7c0ab1648bc51fe33c5dbc23c69003e0796f5349
rjwboys <rjwboys@att.net>
2013-11-26 22:04:17 -0600

    Update findconflcit.bat
    oops fixed again

--- findconflcit.bat
+++ findconflcit.bat
@@ -9,7 +9,7 @@ REM Sets varDD to the Day
 SET varDD=%DATE:~7,2%
 REM set hr=%TIME:~0,2%
 REM Sets the Variable for Conflicts to YourDesktop\conflicts\Year\Month\Day\
-SET Conflicts=%DirBase%\conflicts\%varYYYY%\%varMM%\%varDD%\ahcraft
+SET Conflicts="%DirBase%\conflicts\%varYYYY%\%varMM%\%varDD%\ahcraft
 REM Checks if the DIR is already made
 IF exist %Conflicts% ( echo Conflicts exists ) ELSE ( mkdir %Conflicts% && echo Conflicts created)
 REM Sets the variable for str2 to CONFLICTS @
